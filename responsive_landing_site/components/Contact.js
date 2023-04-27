afca6c70bf3627bdf2ac492189f1fddcf99daedb
ppyaesone <white.soxpps@gmail.com>
2023-04-27 15:24:47 +0630

    final_design

---
+++ responsive_landing_site/components/Contact.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="max-w-[1240px] m-auto p-4 h-screen">
+      <h1 className="text-2xl font-bold text-center p-4">
+        Let's Work Together!
+      </h1>
+      <form className="max-w-[600px] m-auto">
+        <div className="grid grid-cols-2 gap-2">
+          <input
+            className="border shadow-lg p-3"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            className="border shadow-lg p-3"
+            type="text"
+            placeholder="Email"
+          />
+        </div>
+        <input
+          className="border shadow-lg p-3 w-full my-2"
+          type="text"
+          placeholder="Subject"
+        />
+        <textarea
+          className="border shadow-lg p-3 w-full"
+          cols="30"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button
+          className="border shadow-lg p-3 w-full mt-2 hover:bg-[#000000] 
+        hover:text-white transition duration-200"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ responsive_landing_site/pages/work.js
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+import Portfolio from "@/components/Portfolio";
+import react from "react";
+
+const Work = () => {
+    return (
+        <div>
+            <Hero heading="My Work" message="This is my recent work travelling the world" />
+            <Portfolio />
+        </div>
+    )
+}
+
+export default Work
